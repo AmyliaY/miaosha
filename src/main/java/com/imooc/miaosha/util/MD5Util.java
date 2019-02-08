8b59485a639e1aa849eeff4e997cb7051cc581f9
AmyliaY <471816751@qq.com>
2019-02-08 22:06:53 +0800

    auto update

---
+++ src/main/java/com/imooc/miaosha/util/MD5Util.java
@@ -0,0 +1,36 @@
+package com.imooc.miaosha.util;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+/**
+* @author 全恒
+*/
+public class MD5Util {
+	
+	public static String md5(String src){
+		return DigestUtils.md5Hex(src);
+	}
+	
+	private static final String salt = "1a2b3c4d";
+	
+	public static String inputPassToFormPass(String inputPass){
+		String str = "" + salt.charAt(0) + salt.charAt(2) + inputPass + salt.charAt(5) + salt.charAt(3);
+		return md5(str);
+	}
+	
+	public static String formPassToDBPass(String formPass, String salt){
+		String str = "" + salt.charAt(0) + salt.charAt(2) + formPass + salt.charAt(5) + salt.charAt(3);
+		return md5(str);
+	}
+	
+	public static String inputPassToDbPass(String input, String saltDB){
+		String formPass = inputPassToFormPass(input);
+		String dbPass = formPassToDBPass(formPass, saltDB);
+		return dbPass;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(inputPassToFormPass("123456"));
+	}
+	
+}
