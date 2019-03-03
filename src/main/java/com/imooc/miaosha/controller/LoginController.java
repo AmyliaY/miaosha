fdce54fd5cd4f279690160648a22be5df8e9c425
AmyliaY <471816751@qq.com>
2019-03-03 10:53:08 +0800

    auto update

--- src/main/java/com/imooc/miaosha/controller/LoginController.java
+++ src/main/java/com/imooc/miaosha/controller/LoginController.java
@@ -39,15 +39,6 @@ public class LoginController {
 	@ResponseBody
 	public Result<Boolean> doLogin(HttpServletResponse response, @Valid LoginVo loginVo){
 		log.info(loginVo.toString());
-		//参数校验
-/*		String passInput = loginVo.getPassword();
-		String mobile = loginVo.getMobile();
-		if(StringUtils.isEmpty(passInput))
-			return Result.error(CodeMsg.PASSWORD_EMPTY);
-		if(StringUtils.isEmpty(mobile))
-			return Result.error(CodeMsg.MOBILE_EMPTY);
-		if(!ValidatorUtil.isMobile(mobile))
-			return Result.error(CodeMsg.MOBILE_ERROR);*/
 		//登陆
 		userService.login(response, loginVo);
 		return Result.success(true);
